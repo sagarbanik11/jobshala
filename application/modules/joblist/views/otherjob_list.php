c7259a6049d6720c0fc799c4147b4f6ebfc9c9bd
Sumit Banik <sumitbanik@groveus.com>
2020-08-01 02:40:56 +0530

    Others Job List

--- application/modules/joblist/views/otherjob_list.php
+++ application/modules/joblist/views/otherjob_list.php
@@ -1,25 +1,51 @@
-<div class="container">
-<br>
- <legend >Job List</legend>
- 
- <div class="list-group">
+<style>
+  .list-group {
+    cursor: none;
+  }
 
-<?php
-        $uri = 'https://api.indeed.com/ads/apisearch?publisher=2849821278263879&q='.$_POST['job'].'&l='.$_POST['loc'].'%2C&format=json&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=India&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2';
-        $jsondata = file_get_contents($uri);
-	$json = json_decode($jsondata, True);
-	$len = count($json['results']);
-?>
- <?php for($x = 0; $x<$len; $x++){?>
-  <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
-    <div class="d-flex w-100 justify-content-between">
-    <h5 class="mb-1"><?=$json['results'][$x]['jobtitle']?></h5>
-    <small></small>
-    </div>
-    <p class="mb-1"><?=$json['results'][$x]['source']?></p>
-    <small><?=$json['results'][$x]['city']?></small>
-    <button onclick="window.location.href='<?=$json['results'][$x]['url']?>'" type="button" class="btn btn-dark float-right">View More</button>
-  </a>
-  <?php }?>
-</div>
+  .job-block {
+    float: left;
+    padding: 60px 0;
+    position: relative;
+    width: 100%;
+    z-index: 1;
+  }
+
+  .job-listing {
+    float: left;
+    width: 100%;
+    display: table;
+    padding: 30px 10px;
+    background: #fff;
+    border-left: 2px solid #fff;
+    padding-right: 30px;
+  }
+</style>
+
+<div class="container">
+  <br>
+  <legend>Job List</legend>
+
+  <div class="list-group">
+
+    <?php
+    $uri = 'https://api.indeed.com/ads/apisearch?publisher=2849821278263879&q=' . $_POST['job'] . '&l=' . $_POST['loc'] . '%2C&format=json&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=India&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2';
+    $jsondata = file_get_contents($uri);
+    $json = json_decode($jsondata, True);
+    $len = count($json['results']);
+    ?>
+    <?php for ($x = 0; $x < $len; $x++) { ?>
+      <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1"><?= $json['results'][$x]['jobtitle'] ?></h5>
+          <small></small>
+        </div>
+        <p class="mb-1"><?= $json['results'][$x]['source'] ?></p>
+        <small><?= $json['results'][$x]['city'] ?></small>
+        <button onclick="window.location.href='<?= $json['results'][$x]['url'] ?>'" 
+        target="_blank"
+        type="button" class="btn btn-info float-right">View More</button>
+      </a>
+    <?php } ?>
+  </div>
 </div>

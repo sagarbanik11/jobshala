5b589a12a1a8577e277767f80f79c88b57ae746b
Sumit Banik <sumitbanik@groveus.com>
2020-08-01 02:41:15 +0530

    Other Jobs View

--- application/modules/joblist/views/otherjobs_view.php
+++ application/modules/joblist/views/otherjobs_view.php
@@ -1,13 +1,16 @@
-<div class="container">
+<div class="container" style="margin:0px auto;text-align:center;">
 <br>
  <legend >Other Jobs</legend>
 <br>
 <form name="search" method="POST" action="otherjob_list">
+	<label>Enter Job Role</label>
 	<input name="job" type="Text">
 	<br>
+	<label>Enter Location</label>
+
 	<input name="loc" type="Text">
 	<br>
-	<Input name ="srch" type="Submit" value="Click">
+	<input name ="srch" type="Submit" value="Search" class="btn btn-info">
 </form>
  
 </div>
